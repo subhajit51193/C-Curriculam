c28071ac58379f67129f35c0698ba93d7704dd04
subhajit51193 <nnorth87@gmail.com>
2023-07-17 19:56:39 +0530

    updated

---
+++ ArithmeticOperators/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Assignment1
+{
+
+    public class Assignment1
+    {
+
+        public static void Main(String[] args)
+        {
+
+            //Question number 1 Basic Arithmetic operations
+            //declaring variables
+            int Number1;
+            int Number2;
+            int Result1;
+            int Result2;
+            int Result3;
+            int Result4;
+
+            //taking first input
+            Console.WriteLine("please enter the Number1");
+
+            //after converting to int storing the input
+            Number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the Number2");
+
+            //after converting to int storing the input
+            Number2 = Convert.ToInt32(Console.ReadLine());
+
+            //addition
+            Result1 = Number1 + Number2;
+
+            //display result for addition
+            Console.WriteLine("Sum of two Numbers:" + Result1.ToString());
+            Console.ReadLine();
+
+            //subtraction
+            Result2 = Number1 - Number2;
+
+            //display result for subtraction
+            Console.WriteLine("Subtraction of two Numbers:" + Result2.ToString());
+            Console.ReadLine();
+
+            //Multiplication
+            Result3 = Number1 * Number2;
+
+            //display result for subtraction
+            Console.WriteLine("Multiplication of two Numbers:" + Result3.ToString());
+            Console.ReadLine();
+
+            //Division
+            Result4 = Number1 / Number2;
+
+            //display result for division
+            Console.WriteLine("Multiplication of two Numbers:" + Result4.ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("ENd of Question number 1.");
+
+        }
+    }
+}
+
---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class Assignment1
+    {
+
+        public static void Main(String[] args)
+        {
+
+            //declaring variable
+            int num;
+            Console.WriteLine("Please Enter number");
+
+            //converting to int
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //checking conditions
+
+            if (num < 0)
+            {
+                Console.WriteLine("The number " + num + " is negetive");
+            }
+            else if(num > 0)
+            {
+                Console.WriteLine("The number " + num + " is postive");
+
+            }
+            else
+            {
+                Console.WriteLine("The number " + num + " is 0");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
